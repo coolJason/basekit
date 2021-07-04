873f872ea801c1f1d39c024f48c2173426e9118c
Rico <luocj@zeasn.com>
2021-07-04 15:23:16 +0800

    BaseViewModel 增加getMutable 而不需要在基类去生成实例

--- base_kit/src/main/java/com/moregood/kit/base/BaseViewModel.java
+++ base_kit/src/main/java/com/moregood/kit/base/BaseViewModel.java
@@ -1,10 +1,16 @@
 package com.moregood.kit.base;
 
+import android.text.TextUtils;
+
+import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
 import com.jeremyliao.liveeventbus.LiveEventBus;
 import com.jeremyliao.liveeventbus.core.Observable;
 import com.moregood.kit.utils.Logger;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Author: Devin.Ding
@@ -13,12 +19,36 @@ import com.moregood.kit.utils.Logger;
  */
 public class BaseViewModel extends ViewModel {
 
+    Map<String, MutableLiveData> mMutableMap;
+
     public BaseViewModel() {
+        mMutableMap = new HashMap<>();
+    }
+
+    /**
+     * 获取所需要的MutableLiveData
+     *
+     * @param key
+     * @param t
+     * @param <T>
+     * @return
+     */
+    public <T> MutableLiveData<T> getMutable(String key, Class<T> t) {
+        if (mMutableMap != null && !TextUtils.isEmpty(key) && t != null) {
+            if (mMutableMap.containsKey(key)) {
+                return mMutableMap.get(key);
+            }
+            MutableLiveData<T> mutableLiveData = new MutableLiveData<>();
+            mMutableMap.put(key, mutableLiveData);
+            return mutableLiveData;
+        }
+        return null;
     }
 
     @Override
     protected void onCleared() {
         super.onCleared();
-        Logger.d("this vm  oncleared...." + this);
+        mMutableMap.clear();
+        mMutableMap = null;
     }
 }
