c8511588c49dd365f4c86303a947053c2e025eef
Rico <luocj@zeasn.com>
2021-08-03 18:02:54 +0800

    ..

--- base_kit/src/main/java/com/moregood/kit/net/BaseHttpMethods.java
+++ base_kit/src/main/java/com/moregood/kit/net/BaseHttpMethods.java
@@ -82,24 +82,24 @@ public abstract class BaseHttpMethods<HS extends IHttpService> {
             }
 
         });
-        HttpLoggingInterceptor loggingInterceptor = new HttpLoggingInterceptor(new HttpLoggingInterceptor.Logger() {
-            String url;
-
-            @Override
-            public void log(String message) {
-                if (message != null) {
-                    if (message.contains("-->") && !message.contains("--> END")) {
-                        url = message;
-                    } else if (message.contains("{\"data\":")) {
-                    }
-                }
-                if(message.startsWith("{\"code")) {
-                    Logger.d("请求原始结果： %s", message);
-                }
-            }
-        });
-        loggingInterceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
-        client.addInterceptor(loggingInterceptor);
+//        HttpLoggingInterceptor loggingInterceptor = new HttpLoggingInterceptor(new HttpLoggingInterceptor.Logger() {
+//            String url;
+//
+//            @Override
+//            public void log(String message) {
+//                if (message != null) {
+//                    if (message.contains("-->") && !message.contains("--> END")) {
+//                        url = message;
+//                    } else if (message.contains("{\"data\":")) {
+//                        LogUtils.logNetResponse(url, message);
+//
+//                    }
+//                }
+//                Logger.e("请求原始结果： %s", message);
+//            }
+//        });
+//        loggingInterceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+//        client.addInterceptor(loggingInterceptor);
         OkHttpClient httpClient = client.build();
         OkHttpClient.Builder httpClientBuilder = new OkHttpClient.Builder();
         httpClientBuilder.connectTimeout(DEFAULT_TIMEOUT, TimeUnit.SECONDS);
@@ -133,9 +133,23 @@ public abstract class BaseHttpMethods<HS extends IHttpService> {
         }
     }
 
+    protected String createListRequestBodyForString(List list) {
+        StringBuilder sb = new StringBuilder("[");
+        if(list!=null&&list.size()>0){
+            for(Object o:list){
+                if(sb.length()>1){
+                    sb.append(",");
+                }
+                sb.append(o.toString());
+            }
+        }
+        sb.append("]");
+        return sb.toString();
+    }
+
     protected RequestBody createListRequestBody(List list) {
         StringBuilder sb = new StringBuilder("[");
-        if(list.size()>0){
+        if(list!=null&&list.size()>0){
             for(Object o:list){
                 if(sb.length()>1){
                     sb.append(",");
