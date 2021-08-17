c49174f39baf867a0b34354e833f49c598ec2aa4
Lin123! <linmj>
2021-08-17 17:13:27 +0800

    推送自定义字段修改

---
+++ base_kit/src/main/java/com/moregood/kit/bean/PushInfo.java
@@ -0,0 +1,22 @@
+package com.moregood.kit.bean;
+
+public class PushInfo {
+    private String content="";
+    private String type="";
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+}
