5343e0edf49b777b6d160c92efec5a1ef19ebcc7
Lin123! <linmj>
2021-08-19 23:11:05 +0800

    地址选择修改

--- base_kit/src/main/java/com/moregood/kit/bean/PushInfo.java
+++ base_kit/src/main/java/com/moregood/kit/bean/PushInfo.java
@@ -1,16 +1,14 @@
 package com.moregood.kit.bean;
 
 public class PushInfo {
-    private String content="";
     private String type="";
+    private String phoneNum;
+    private String countryNo;
+    private String orderNo;
+    private String userName;
 
-    public String getContent() {
-        return content;
-    }
 
-    public void setContent(String content) {
-        this.content = content;
-    }
+
 
     public String getType() {
         return type;
@@ -18,5 +16,37 @@ public class PushInfo {
 
     public void setType(String type) {
         this.type = type;
+    }
+
+    public String getPhoneNum() {
+        return phoneNum;
+    }
+
+    public void setPhoneNum(String phoneNum) {
+        this.phoneNum = phoneNum;
+    }
+
+    public String getCountryNo() {
+        return countryNo;
+    }
+
+    public void setCountryNo(String countryNo) {
+        this.countryNo = countryNo;
+    }
+
+    public String getOrderNo() {
+        return orderNo;
+    }
+
+    public void setOrderNo(String orderNo) {
+        this.orderNo = orderNo;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
     }
 }
