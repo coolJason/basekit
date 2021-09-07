b5fa5e02c2660581ed2d6354c9ae7c06cbeadce1
linmj <Lin>
2021-09-07 20:58:48 +0800

    我的消息  联系客服

---
+++ base_kit/src/main/java/com/moregood/kit/utils/WebConstant.java
@@ -0,0 +1,9 @@
+package com.moregood.kit.utils;
+
+public class WebConstant {
+    public static String MERCHANT_HELP="https://api.baituomall.com/help/index.html#/storeHelp";//商家端帮助中心
+    public static String MERCHANT_CUSTOMER="https://api.baituomall.com/help/index.html#/storeService";//商家端客服服务
+    public static String USER_FREQUENTLY_ASKED_QUESTIOS="https://api.baituomall.com/help/index.html#/userHelp?id=14";//用户端常见问题
+    public static String USER_CUSTOMER="https://api.baituomall.com/help/index.html#/userService?id=14";//商家端客服服务
+    public static String RIDER_CUSTOMER="https://api.baituomall.com/help/index.html#/riderService";//骑手端客服服务
+}
