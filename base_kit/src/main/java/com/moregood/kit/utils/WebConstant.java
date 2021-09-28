a28eff095358314fb9a9e5ffc90ecdd12284bed4
luxz <526986111@qq.com>
2021-09-28 18:26:27 +0800

    1、新增用户协议和隐私协议

--- base_kit/src/main/java/com/moregood/kit/utils/WebConstant.java
+++ base_kit/src/main/java/com/moregood/kit/utils/WebConstant.java
@@ -6,4 +6,12 @@ public class WebConstant {
     public static String USER_FREQUENTLY_ASKED_QUESTIOS="https://api.baituomall.com/help/index.html#/userHelp?id=14";//用户端常见问题
     public static String USER_CUSTOMER="https://api.baituomall.com/help/index.html#/userService?id=14";//商家端客服服务
     public static String RIDER_CUSTOMER="https://api.baituomall.com/help/index.html#/riderService";//骑手端客服服务
+
+
+    public static final String MALL_SERVICE_AGREEMENT = "https://www.baituomall.com/help/index.html#/agreement?type=0";    //用户端服务协议
+    public static final String MALL_PRIVACY_AGREEMENT = "https://www.baituomall.com/help/index.html#/agreement?type=1";    //用户端隐私协议
+    public static final String MERCHANT_SERVICE_AGREEMENT = "https://www.baituomall.com/help/index.html#/agreement?type=2";//商家端服务协议
+    public static final String MERCHANT_PRIVACY_AGREEMENT = "https://www.baituomall.com/help/index.html#/agreement?type=3";//商家端隐私协议
+    public static final String RIDER_SERVICE_AGREEMENT = "https://www.baituomall.com/help/index.html#/agreement?type=4";   //骑手端服务协议
+    public static final String RIDER_PRIVACY_AGREEMENT = "https://www.baituomall.com/help/index.html#/agreement?type=5";   //骑手端隐私协议
 }
