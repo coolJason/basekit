dddf292a047196edc1426971c6ccf2b2ba12252c
Xiezw <862292012@qq.com>
2021-10-26 09:44:55 +0800

    推送优化

---
+++ base_kit/src/main/java/com/moregood/kit/bean/BaiTuoPush.java
@@ -0,0 +1,34 @@
+package com.moregood.kit.bean;
+
+/**
+ * 推送内容
+ */
+public class BaiTuoPush {
+    private String title;
+    private String content;
+    private String custom_content;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getCustom_content() {
+        return custom_content;
+    }
+
+    public void setCustom_content(String custom_content) {
+        this.custom_content = custom_content;
+    }
+}
