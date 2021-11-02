719a4aab9d48233db416e02b1708c3aacd3a5f54
Xiezw <862292012@qq.com>
2021-11-02 09:26:14 +0800

    客服im开发

--- base_kit/src/main/java/com/moregood/kit/net/BaseHttpMethods.java
+++ base_kit/src/main/java/com/moregood/kit/net/BaseHttpMethods.java
@@ -72,11 +72,11 @@ public abstract class BaseHttpMethods<HS extends IHttpService> {
                     Logger.d("未 add header");
                 }
                 builder.addHeader("Content-Type", "application/json")
-                        .addHeader("Connection", "keep-alive")
+//                        .addHeader("Connection", "keep-alive")
                         .addHeader("Accept", "application/json")
-                        .addHeader("Access-Control-Allow-Origin", "*")
-                        .addHeader("Access-Control-Allow-Headers", "X-Requested-With")
-                        .addHeader("Vary", "Accept-Encoding")
+//                        .addHeader("Access-Control-Allow-Origin", "*")
+//                        .addHeader("Access-Control-Allow-Headers", "X-Requested-With")
+//                        .addHeader("Vary", "Accept-Encoding")
                         .method(original.method(), original.body());
                 Request request = builder.build();
                 return chain.proceed(request);
@@ -96,7 +96,8 @@ public abstract class BaseHttpMethods<HS extends IHttpService> {
 //
 //                    }
 //                }
-                Logger.e("请求原始结果： %s", message);
+//                Logger.e("请求原始结果： %s", message);
+                Logger.e(message);
             }
         });
         loggingInterceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
