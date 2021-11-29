a57ed0ea16dc0348f9cd722422df37e27803096b
luxz <526986111@qq.com>
2021-11-29 15:07:10 +0800

    1、提交WebViewActivity桥接方法2、header新增电话和用户名3、店铺小购物车跳转大购物车去掉满多少起送的限制

--- base_kit/src/main/java/com/moregood/kit/ui/data/Header.java
+++ base_kit/src/main/java/com/moregood/kit/ui/data/Header.java
@@ -6,6 +6,8 @@ public class Header {
     private String userType;
     private String userId;
     private String token;
+    private String phoneNumber;
+    private String userName;
 
     public String getExpireTime() {
         return expireTime;
@@ -45,5 +47,21 @@ public class Header {
 
     public void setToken(String token) {
         this.token = token;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
     }
 }
