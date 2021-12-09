7218dddf4d703df012687a3e82c0a4eee1b6c1ad
Xiezw <862292012@qq.com>
2021-12-09 11:23:57 +0800

    1.增加评价功能

--- base_kit/src/main/java/com/moregood/kit/utils/Constans.java
+++ base_kit/src/main/java/com/moregood/kit/utils/Constans.java
@@ -3,4 +3,7 @@ package com.moregood.kit.utils;
 public interface Constans {
     String STORE_ID = "storeId";
     String CLOSE_DIALOG = "closeDialog";
+    String MY_COMMENT = "myComment";
+    String COMMENT_ID = "commentId";
+    String DEL_COMMENT = "delComment";
 }
