2be5e18465fef4f1f7912a83b22e574eb2e3f7f8
luxz <526986111@qq.com>
2022-01-18 15:23:42 +0800

    1、防止重复点击移到basekit

---
+++ base_kit/src/main/java/com/moregood/kit/utils/ClickUtils.java
@@ -0,0 +1,17 @@
+package com.moregood.kit.utils;
+
+public class ClickUtils {
+        // 两次点击按钮之间的点击间隔不能少于1000毫秒
+        private static final int MIN_CLICK_DELAY_TIME = 500;
+        private static long lastClickTime;
+     
+        public static boolean isFastClick() {
+            boolean flag = false;
+            long curClickTime = System.currentTimeMillis();
+            if ((curClickTime - lastClickTime) >= MIN_CLICK_DELAY_TIME) {
+                lastClickTime = curClickTime;
+                flag = true;
+            }
+            return flag;
+        }
+    }
