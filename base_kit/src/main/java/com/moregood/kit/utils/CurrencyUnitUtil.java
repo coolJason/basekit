49aa14433d0b1cb5d9f5fbffe48c795568ca09ba
SVTAR <g.svtar@gmail.com>
2022-07-28 20:42:04 +0800

    1.货币单位工具类

---
+++ base_kit/src/main/java/com/moregood/kit/utils/CurrencyUnitUtil.java
@@ -0,0 +1,41 @@
+package com.moregood.kit.utils;
+
+import android.text.TextUtils;
+
+/**
+ * Author: Svtar
+ * Date: 2022/7/28 14:51
+ * E-mail: 707390415@qq.com
+ * Description: 货币单位工具类
+ */
+public class CurrencyUnitUtil {
+
+    private static final String KEY_CURRENCY_UNI = "KEY_CURRENCY_UNI";
+    private static String unit = "";
+
+    /**
+     * 更新货币单位
+     *
+     * @param unit 需要更新的单位
+     */
+    public static void updateUnit(String unit) {
+        if(TextUtils.isEmpty(unit)){
+            return;
+        }
+        MmkvUtil.put(KEY_CURRENCY_UNI, unit);
+        CurrencyUnitUtil.unit = unit;
+    }
+
+    /**
+     * 获取货币单位
+     *
+     * @return
+     */
+    public static String getUnit() {
+        if (TextUtils.isEmpty(unit)) {
+            unit = MmkvUtil.getString(KEY_CURRENCY_UNI, "AED");
+        }
+        return unit;
+    }
+
+}
