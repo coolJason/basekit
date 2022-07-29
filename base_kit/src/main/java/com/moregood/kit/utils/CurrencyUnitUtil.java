e35b19998d0c0d05ed9c8756ca0a3733ef32f458
SVTAR <g.svtar@gmail.com>
2022-07-29 15:54:42 +0800

    1.修复空指针

--- base_kit/src/main/java/com/moregood/kit/utils/CurrencyUnitUtil.java
+++ base_kit/src/main/java/com/moregood/kit/utils/CurrencyUnitUtil.java
@@ -19,7 +19,7 @@ public class CurrencyUnitUtil {
      * @param unit 需要更新的单位
      */
     public static void updateUnit(String unit) {
-        if(TextUtils.isEmpty(unit)){
+        if (TextUtils.isEmpty(unit)) {
             return;
         }
         MmkvUtil.put(KEY_CURRENCY_UNI, unit);
@@ -34,6 +34,18 @@ public class CurrencyUnitUtil {
     public static String getUnit() {
         if (TextUtils.isEmpty(unit)) {
             unit = MmkvUtil.getString(KEY_CURRENCY_UNI, "AED");
+        }
+        return unit;
+    }
+
+    /**
+     * 检查货币单位是否为空，如果是空返回默认的
+     *
+     * @return
+     */
+    public static String checkUnit(String unit) {
+        if (TextUtils.isEmpty(unit)) {
+            return "AED";
         }
         return unit;
     }
--- base_kit/src/main/java/com/moregood/kit/widget/CurrencyTextView.java
+++ base_kit/src/main/java/com/moregood/kit/widget/CurrencyTextView.java
@@ -3,7 +3,6 @@ package com.moregood.kit.widget;
 import android.content.Context;
 import android.text.Spannable;
 import android.util.AttributeSet;
-import android.util.Log;
 
 import androidx.annotation.Nullable;
 
@@ -32,14 +31,18 @@ public class CurrencyTextView extends androidx.appcompat.widget.AppCompatTextVie
 
     @Override
     public void setText(CharSequence text, BufferType type) {
-        String newText =text.toString();
-        if(text.toString().toLowerCase().contains("aed")&& !CurrencyUnitUtil.getUnit().equalsIgnoreCase("aed")){
-            StringBuilder sb = new StringBuilder(text.length());
-            sb.append(text);
-            newText =  sb.toString().replaceAll("aed", CurrencyUnitUtil.getUnit()).replaceAll("AED",CurrencyUnitUtil.getUnit());
+        if(text!=null){
+            String newText =text.toString();
+            if(text.toString().toLowerCase().contains("aed")&& !CurrencyUnitUtil.getUnit().equalsIgnoreCase("aed")){
+                StringBuilder sb = new StringBuilder(text.length());
+                sb.append(text);
+                newText =  sb.toString().replaceAll("aed", CurrencyUnitUtil.getUnit()).replaceAll("AED",CurrencyUnitUtil.getUnit());
+            }
+            Spannable s = getCustomSpannableString(getContext(), newText);
+            super.setText(s, BufferType.SPANNABLE);
+        }else{
+            super.setText(text,type);
         }
-        Spannable s = getCustomSpannableString(getContext(), newText);
-        super.setText(s, BufferType.SPANNABLE);
     }
 
     private static Spannable getCustomSpannableString(Context context, CharSequence text) {
