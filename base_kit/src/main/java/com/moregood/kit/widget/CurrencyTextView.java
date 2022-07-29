c2f5e2742981d4ee861c7411e3f0e3c059885b42
zhongwei <zhongwei@baituo.com>
2022-07-29 10:41:43 +0800

    动态货币符号

---
+++ base_kit/src/main/java/com/moregood/kit/widget/CurrencyTextView.java
@@ -0,0 +1,50 @@
+package com.moregood.kit.widget;
+
+import android.content.Context;
+import android.text.Spannable;
+import android.util.AttributeSet;
+import android.util.Log;
+
+import androidx.annotation.Nullable;
+
+import com.moregood.kit.utils.CurrencyUnitUtil;
+
+/**
+ * 自动更换货币符号
+ */
+public class CurrencyTextView extends androidx.appcompat.widget.AppCompatTextView {
+    public CurrencyTextView(Context context) {
+        super(context);
+
+    }
+
+    public CurrencyTextView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+
+    }
+
+    public CurrencyTextView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+
+    }
+
+    private static final Spannable.Factory spannableFactory = Spannable.Factory.getInstance();
+
+    @Override
+    public void setText(CharSequence text, BufferType type) {
+        String newText =text.toString();
+        if(text.toString().toLowerCase().contains("aed")&& !CurrencyUnitUtil.getUnit().equalsIgnoreCase("aed")){
+            StringBuilder sb = new StringBuilder(text.length());
+            sb.append(text);
+            newText =  sb.toString().replaceAll("aed", CurrencyUnitUtil.getUnit()).replaceAll("AED",CurrencyUnitUtil.getUnit());
+        }
+        Spannable s = getCustomSpannableString(getContext(), newText);
+        super.setText(s, BufferType.SPANNABLE);
+    }
+
+    private static Spannable getCustomSpannableString(Context context, CharSequence text) {
+        Spannable spannable = spannableFactory.newSpannable(text);
+        return spannable;
+    }
+
+}
